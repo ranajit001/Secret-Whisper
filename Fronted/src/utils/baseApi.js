88a859e809ab2910e7355ffbf21046c8fb91ec58
Ranajit Bangal <ranajitbangal01@gmail.com>
2025-08-28 17:04:04 +0530

    Update baseApi.js

--- Fronted/src/utils/baseApi.js
+++ Fronted/src/utils/baseApi.js
@@ -1,2 +1,2 @@
-// export const baseApi = `https://secret-whisper-xnnk.onrender.com`
-export const baseApi = `http://localhost:3000`
+ export const baseApi = `https://secret-whisper-862g.onrender.com`
+// export const baseApi = `http://localhost:3000`
